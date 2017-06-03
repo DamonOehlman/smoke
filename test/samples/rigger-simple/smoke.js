64eab32d15c2022a8672f471eb346fe2bfa2e784
Damon Oehlman <damon.oehlman@gmail.com>
2017-06-03 13:38:47 +1000

    removed old files

--- test/samples/rigger-simple/smoke.js
+++
@@ -1,9 +0,0 @@
-var rigger = require('rigger'),
-    fs = require('fs');
-
-// smoke all source files in the src/js path
-smoke('src/js/')
-    // on any js file, rig it and create a dist file
-    .match(/\.js$/i)
-        .pipe(new rigger.Rigger())
-        .pipe(fs.createWriteStream('dist/{{ filename }}'));
