5de1cfe27951b73f980463073c221b3388cd7560
Damon Oehlman <damon.oehlman@gmail.com>
2018-03-01 12:32:28 +1100

    more detail in the st type definitions

--- typings/st.d.ts
+++ typings/st.d.ts
@@ -2,15 +2,30 @@ declare module 'st' {
   import { IncomingMessage, ServerResponse } from 'http';
 
   namespace StaticFileServer {
+    type MountFunction = (options: StaticMountOptions) => RequestHandler;
+    type RequestHandler = (req: IncomingMessage, res: ServerResponse, passthrough?: () => {}) => void;
+
     type StaticMountOptions = {
       path: string;
       url?: string;
       passthrough?: boolean;
       cors?: boolean;
+      index?: boolean | string;
+      cache?: boolean | CacheOptions;
     };
 
-    type RequestHandler = (req: IncomingMessage, res: ServerResponse, passthrough?: () => {}) => void;
-    type MountFunction = (options: StaticMountOptions) => RequestHandler;
+    type CacheOptions = {
+      fd?: CacheAgeOptions;
+      stat?: CacheAgeOptions;
+      content?: CacheAgeOptions & { cacheControl: string };
+      index?: CacheAgeOptions;
+      readdir?: CacheAgeOptions;
+    };
+
+    type CacheAgeOptions = {
+      max: number;
+      maxAge: number;
+    };
   }
 
   var st: StaticFileServer.MountFunction;
